cb0336cf94b3f7e988909ae585445ad1ee2a3f0c
Tomas Votruba <tomas.vot@gmail.com>
2022-08-18 00:44:24 +0200

    fixup! misc

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,11 +4,14 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Table(name: 'fos_user')]
+/**
+ * @ORM\Table(name="fos_user")
+ */
 class User
 {
-    #[ORM\Id]
-    #[ORM\Column(type: 'integer')]
-    #[ORM\GeneratedValue(strategy: 'AUTO')]
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     */
     protected $id;
 }
