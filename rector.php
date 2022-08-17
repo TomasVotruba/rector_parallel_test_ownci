b69e999944f7c0dcb2a3249481c5e2adbd1f873f
Tomas Votruba <tomas.vot@gmail.com>
2022-08-18 00:47:49 +0200

    misc

--- rector.php
+++ rector.php
@@ -1,19 +1,10 @@
 <?php
+
 use Rector\Doctrine\Set\DoctrineSetList;
-use Rector\Symfony\Set\SymfonySetList;
-use Rector\Symfony\Set\SensiolabsSetList;
-use Rector\Nette\Set\NetteSetList;
 use Rector\Config\RectorConfig;
 
 return function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
-        __DIR__ . '/src/Entity',
-    ]);
-
     $rectorConfig->sets([
         DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
-//        SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
-//        NetteSetList::ANNOTATIONS_TO_ATTRIBUTES,
-//        SensiolabsSetList::FRAMEWORK_EXTRA_61,
     ]);
 };
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,9 +9,4 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class User
 {
-    /**
-     * @ORM\Id
-     * @ORM\Column(type="integer")
-     */
-    protected $id;
 }
