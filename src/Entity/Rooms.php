9c1db34f2d6645cdb714e919f0385cd5cb8a31ac
Tomas Votruba <tomas.vot@gmail.com>
2022-08-18 00:11:20 +0200

    remove few methods

--- src/Entity/Rooms.php
+++ src/Entity/Rooms.php
@@ -22,29 +22,9 @@ class Rooms
     private $id;
 
     /**
-     * @ORM\Column(type="text")
-     */
-    private $name;
-
-    /**
-     * @ORM\Column(type="datetime", nullable=true)
-     */
-    private $start;
-
-    /**
-     * @ORM\Column(type="datetime", nullable=true)
-     */
-    private $enddate;
-
-    /**
      * @ORM\ManyToMany(targetEntity=User::class, inversedBy="rooms")
      */
     private $user;
-
-    /**
-     * @ORM\Column(type="text")
-     */
-    private $uid;
 
     /**
      * @ORM\ManyToOne(targetEntity=User::class, inversedBy="roomModerator")
@@ -59,45 +39,9 @@ class Rooms
     private $prototypeUsers;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $timeZone;
-
-    /**
-     * @ORM\Column(type="datetime", nullable=true)
-     */
-    private $startUtc;
-
-    /**
-     * @ORM\Column(type="datetime", nullable=true)
-     */
-    private $endDateUtc;
-
-    /**
      * @ORM\ManyToMany(targetEntity=User::class, mappedBy="favorites")
      */
     private $favoriteUsers;
-
-    /**
-     * @ORM\Column(type="boolean", nullable=true)
-     */
-    private $lobby;
-
-    /**
-     * @ORM\Column(type="integer", nullable=true)
-     */
-    private $startTimestamp;
-
-    /**
-     * @ORM\Column(type="integer", nullable=true)
-     */
-    private $endTimestamp;
-
-    /**
-     * @ORM\Column(type="text", nullable=true)
-     */
-    private $hostUrl;
-
 
     public function __construct()
     {
@@ -105,30 +49,6 @@ class Rooms
         $this->userAttributes = new ArrayCollection();
         $this->prototypeUsers = new ArrayCollection();
         $this->favoriteUsers = new ArrayCollection();
-    }
-
-    public function getStartUtc(): ?\DateTimeInterface
-    {
-        return new \DateTime($this->startUtc->format('Y-m-d H:i:s'), new \DateTimeZone('utc'));
-    }
-
-    public function setStartUtc(?\DateTimeInterface $startUtc): self
-    {
-        $this->startUtc = $startUtc;
-
-        return $this;
-    }
-
-    public function getEndDateUtc(): ?\DateTimeInterface
-    {
-        return new \DateTime($this->endDateUtc->format('Y-m-d H:i:s'), new \DateTimeZone('utc'));
-    }
-
-    public function setEndDateUtc(?\DateTimeInterface $endDateUtc): self
-    {
-        $this->endDateUtc = $endDateUtc;
-
-        return $this;
     }
 
     /**
