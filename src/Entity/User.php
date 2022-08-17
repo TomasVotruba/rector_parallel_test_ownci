cb3e40a27db0b104de0e1a81e98c15c1f13002f4
Tomas Votruba <tomas.vot@gmail.com>
2022-08-18 00:39:32 +0200

    bump deps

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,8 +2,6 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -17,41 +15,4 @@ class User
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
-
-    /**
-     * @ORM\ManyToMany(targetEntity=Rooms::class, mappedBy="user")
-     */
-    private $rooms;
-
-    public function __construct()
-    {
-        $this->rooms = new ArrayCollection();
-    }
-
-    /**
-     * @return Collection|Rooms[]
-     */
-    public function getRooms(): Collection
-    {
-        return $this->rooms;
-    }
-
-    public function addRoom(Rooms $room): self
-    {
-        if (!$this->rooms->contains($room)) {
-            $this->rooms[] = $room;
-            $room->addUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeRoom(Rooms $room): self
-    {
-        if ($this->rooms->removeElement($room)) {
-            $room->removeUser($this);
-        }
-
-        return $this;
-    }
 }
